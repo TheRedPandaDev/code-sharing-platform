7f38dfb1ea9467d9e5fe5b50099aa9c7c849eb10
RealThomasMiles <Dmilya2000@gmail.com>
2021-03-26 12:19:58 +0300

    Added an alert box with the newly posted snippet's id

--- src/resources/static/js/codeNewSend.js
+++ src/resources/static/js/codeNewSend.js
@@ -5,12 +5,17 @@ function send() {
 
     let json = JSON.stringify(object);
 
+    let jsonResponse;
+
     let xhr = new XMLHttpRequest();
     xhr.open("POST", '/api/code/new', false)
+    xhr.onload  = function() {
+        jsonResponse = JSON.parse(xhr.responseText);
+    };
     xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     xhr.send(json);
 
     if (xhr.status === 200) {
-        alert("Success!");
+        alert("Success! Snippet ID: " + jsonResponse.id);
     }
 }
